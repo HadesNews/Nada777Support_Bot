e0ce051374cafca427b675e7492f4dfe23a0cd60
Hades News <mrdul14022025193723@e-record.com>
2025-04-26 18:43:14 +0700

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Token bot
-const token = 'ISI_TOKEN_BOT_KAMU_DISINI';
+const token = '7971857678:AAEEjSVxrG850rXeirVNYeTDqjNFWPd-LFU';
 
 // Inisialisasi Bot
 const bot = new TelegramBot(token, { polling: true });
